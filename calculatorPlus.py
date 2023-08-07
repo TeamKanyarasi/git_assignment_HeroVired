b39a3bdb163d89c004f7b6aab4d6dd38241ee7ff
TeamKanyarasi <charanyandrapu09@gmail.com>
2023-08-07 17:35:16 +0530

    bug fixed for divide function

--- calculatorPlus.py
+++ calculatorPlus.py
@@ -8,13 +8,15 @@ class Calculator:
     def multiply(self,a,b):
         return a*b
     def divide(self,a,b):
+        if b == 0:
+            return ValueError("Cannot divide by zero")
         return a/b
     
 if __name__ == "__main__":
     calculator = Calculator()
 
 num1 = 16
-num2 = 4
+num2 = 0
 
 print(f"{num1}+{num2} = {calculator.add(num1,num2)}")
 print(f"{num1}-{num2} = {calculator.subtract(num1,num2)}")
